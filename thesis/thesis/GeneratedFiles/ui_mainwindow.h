e3c26c7f2776085c7f2fc07f090a486d5a0defb6
Roger Hernando Buch <el_tro@hotmail.com>
2015-03-19 12:28:02 +0100

    add glm and libs

---
+++ thesis/thesis/GeneratedFiles/ui_mainwindow.h
@@ -0,0 +1,144 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 5.4.0
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QAction>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QButtonGroup>
+#include <QtWidgets/QFrame>
+#include <QtWidgets/QHBoxLayout>
+#include <QtWidgets/QHeaderView>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenu>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QSpacerItem>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QToolBar>
+#include <QtWidgets/QVBoxLayout>
+#include <QtWidgets/QWidget>
+#include "glwidget.h"
+
+QT_BEGIN_NAMESPACE
+
+class Ui_mainwindowClass
+{
+public:
+    QAction *actionNothing;
+    QAction *actionFoo;
+    QWidget *centralWidget;
+    QHBoxLayout *horizontalLayout;
+    GLWidget *widget;
+    QVBoxLayout *verticalLayout;
+    QLabel *label;
+    QFrame *line;
+    QSpacerItem *verticalSpacer;
+    QMenuBar *menuBar;
+    QMenu *menuFile;
+    QToolBar *mainToolBar;
+    QStatusBar *statusBar;
+
+    void setupUi(QMainWindow *mainwindowClass)
+    {
+        if (mainwindowClass->objectName().isEmpty())
+            mainwindowClass->setObjectName(QStringLiteral("mainwindowClass"));
+        mainwindowClass->resize(680, 571);
+        actionNothing = new QAction(mainwindowClass);
+        actionNothing->setObjectName(QStringLiteral("actionNothing"));
+        actionFoo = new QAction(mainwindowClass);
+        actionFoo->setObjectName(QStringLiteral("actionFoo"));
+        centralWidget = new QWidget(mainwindowClass);
+        centralWidget->setObjectName(QStringLiteral("centralWidget"));
+        horizontalLayout = new QHBoxLayout(centralWidget);
+        horizontalLayout->setSpacing(6);
+        horizontalLayout->setContentsMargins(11, 11, 11, 11);
+        horizontalLayout->setObjectName(QStringLiteral("horizontalLayout"));
+        widget = new GLWidget(centralWidget);
+        widget->setObjectName(QStringLiteral("widget"));
+        QSizePolicy sizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+        sizePolicy.setHorizontalStretch(0);
+        sizePolicy.setVerticalStretch(0);
+        sizePolicy.setHeightForWidth(widget->sizePolicy().hasHeightForWidth());
+        widget->setSizePolicy(sizePolicy);
+
+        horizontalLayout->addWidget(widget);
+
+        verticalLayout = new QVBoxLayout();
+        verticalLayout->setSpacing(6);
+        verticalLayout->setObjectName(QStringLiteral("verticalLayout"));
+        label = new QLabel(centralWidget);
+        label->setObjectName(QStringLiteral("label"));
+        QFont font;
+        font.setFamily(QStringLiteral("MS Shell Dlg 2"));
+        font.setPointSize(12);
+        font.setBold(true);
+        font.setWeight(75);
+        label->setFont(font);
+        label->setLayoutDirection(Qt::LeftToRight);
+        label->setAlignment(Qt::AlignCenter);
+
+        verticalLayout->addWidget(label);
+
+        line = new QFrame(centralWidget);
+        line->setObjectName(QStringLiteral("line"));
+        line->setFrameShape(QFrame::HLine);
+        line->setFrameShadow(QFrame::Sunken);
+
+        verticalLayout->addWidget(line);
+
+        verticalSpacer = new QSpacerItem(20, 40, QSizePolicy::Minimum, QSizePolicy::Expanding);
+
+        verticalLayout->addItem(verticalSpacer);
+
+
+        horizontalLayout->addLayout(verticalLayout);
+
+        mainwindowClass->setCentralWidget(centralWidget);
+        menuBar = new QMenuBar(mainwindowClass);
+        menuBar->setObjectName(QStringLiteral("menuBar"));
+        menuBar->setGeometry(QRect(0, 0, 680, 26));
+        menuFile = new QMenu(menuBar);
+        menuFile->setObjectName(QStringLiteral("menuFile"));
+        mainwindowClass->setMenuBar(menuBar);
+        mainToolBar = new QToolBar(mainwindowClass);
+        mainToolBar->setObjectName(QStringLiteral("mainToolBar"));
+        mainwindowClass->addToolBar(Qt::TopToolBarArea, mainToolBar);
+        statusBar = new QStatusBar(mainwindowClass);
+        statusBar->setObjectName(QStringLiteral("statusBar"));
+        mainwindowClass->setStatusBar(statusBar);
+
+        menuBar->addAction(menuFile->menuAction());
+        menuFile->addAction(actionNothing);
+        menuFile->addAction(actionFoo);
+
+        retranslateUi(mainwindowClass);
+
+        QMetaObject::connectSlotsByName(mainwindowClass);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *mainwindowClass)
+    {
+        mainwindowClass->setWindowTitle(QApplication::translate("mainwindowClass", "mainwindow", 0));
+        actionNothing->setText(QApplication::translate("mainwindowClass", "nothing", 0));
+        actionFoo->setText(QApplication::translate("mainwindowClass", "foo", 0));
+        label->setText(QApplication::translate("mainwindowClass", "Control Panel", 0));
+        menuFile->setTitle(QApplication::translate("mainwindowClass", "File", 0));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class mainwindowClass: public Ui_mainwindowClass {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
