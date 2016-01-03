101657183a7a8ea010e570aa4cae9b9fbef95c0b
Roger Hernando Buch <el_tro@hotmail.com>
2016-01-03 13:54:18 +0100

    rename project

--- thesis/thesis/glslshadermanager.h
+++ thesis/thesis/glslshadermanager.h
@@ -53,28 +53,3 @@ private:
 };
 
 #endif // GLSLSHADERMANAGER_H
-
-//class FileSystem
-//{
-//public:
-//	static FileSystem& instance();
-//
-//	virtual ~FileSystem() {}
-//	virtual char* readFile(char* path) = 0;
-//	virtual void  writeFile(char* path, char* contents) = 0;
-//
-//protected:
-//	FileSystem() {}
-//};
-//The clever part is how the instance is created :
-//
-//FileSystem& FileSystem::instance()
-//{
-//#if PLATFORM == PLAYSTATION3
-//	static FileSystem *instance = new PS3FileSystem();
-//#elif PLATFORM == WII
-//	static FileSystem *instance = new WiiFileSystem();
-//#endif
-//
-//	return *instance;
-//}
